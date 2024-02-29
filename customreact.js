545038f12408b90e487b473c4141a7192b7bfc01
Dinanath <dkmdkm26@gmail.com>
2024-02-29 12:29:26 +0545

    update js function

---
+++ customreact.js
@@ -0,0 +1,19 @@
+function customRender(reactElement, container) {
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+
+  domElement.setAttribute("href", reactElement.props.href);
+  domElement.setAttribute("target", reactElement.props.target);
+  container.appendChild(domElement);
+}
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://dinanath.com.np",
+    target: "_blank",
+  },
+  children: "Click me to visit mywebsite",
+};
+const mainContainer = document.querySelector("#root");
+
+customRender(reactElement, mainContainer);

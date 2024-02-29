760ad0188b7b95b08f2810766411375159ebdd7a
Dinanath <dkmdkm26@gmail.com>
2024-02-29 13:01:25 +0545

    update js function

--- customreact.js
+++ customreact.js
@@ -1,4 +1,4 @@
-function customRender(customElement, container) {
+function customRender(reactElement, container) {
   /*
  
  const domElement = document.createElement(customElement.type);
@@ -9,8 +9,17 @@ function customRender(customElement, container) {
   container.appendChild(domElement);
 
   */
+
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  for (const prop in reactElement.props) {
+    if (prop === "children") continue;
+
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
+  container.appendChild(domElement);
 }
-const customElement = {
+const reactElement = {
   type: "a",
   props: {
     href: "https://www.google.com",
@@ -20,4 +29,4 @@ const customElement = {
 };
 const mainContainer = document.querySelector("#root");
 
-customRender(customElement, mainContainer);
+customRender(reactElement, mainContainer);
